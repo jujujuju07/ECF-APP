d44013e851cf869049c801d0193d40a9223d99fc
jules-fogeron <jules.fogeron@lyceeastier.com>
2024-02-04 16:11:53 +0100

    add request Identification

---
+++ app/src/main/java/com/example/ecf_app/json/Medecin.kt
@@ -0,0 +1,9 @@
+package com.example.ecf_app.json
+
+data class Medecin(
+    val id: Int,
+    val matricule: Int,
+    val nom: String,
+    val prenom: String,
+    val specialite: String
+)
---
+++ app/src/main/java/com/example/ecf_app/retrofit/ApiAdapteur.kt
@@ -0,0 +1,15 @@
+package com.example.ecf_app.retrofit
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiAdapteur {
+    private const val BASE_URL = "http://192.168.1.38/ECF-Web/api/"
+    val apiClient: Api = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(OkHttpClient())
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(Api::class.java)
+}
