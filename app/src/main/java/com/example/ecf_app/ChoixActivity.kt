1a6b9faa153fe15f6aa6ece9ad61d2c17eb7654d
jules-fogeron <jules.fogeron@lyceeastier.com>
2024-02-08 15:10:23 +0100

    activity ChoixActivity

--- app/src/main/java/com/example/ecf_app/ChoixActivity.kt
+++ app/src/main/java/com/example/ecf_app/ChoixActivity.kt
@@ -1,14 +1,40 @@
 package com.example.ecf_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ecf_app.avis.ListAvisActivity
 import com.example.ecf_app.databinding.ActivityChoixBinding
+import com.example.ecf_app.json.ListPatientItem
+import com.example.ecf_app.json.Medecin
+import com.example.ecf_app.prescription.ListPrescriptionActivity
 
 class ChoixActivity : AppCompatActivity() {
     private lateinit var binding: ActivityChoixBinding
+    private lateinit var medecin: Medecin
+    private lateinit var patient: ListPatientItem
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChoixBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        medecin = intent.getSerializableExtra("objetMedecin") as Medecin
+        patient = intent.getSerializableExtra("objetPatient") as ListPatientItem
+
+
+        binding.ChoixActivityConstraintLayoutAvis.setOnClickListener {
+            val intent = Intent(it.context, ListAvisActivity::class.java)
+            lancerActivity(it,intent)
+        }
+        binding.ChoixActivityConstraintLayoutPrescription.setOnClickListener {
+            val intent = Intent(it.context, ListPrescriptionActivity::class.java)
+            lancerActivity(it,intent)
+        }
+    }
+
+    fun lancerActivity(it: View, intent: Intent){
+        intent.putExtra("objetPatient",patient)
+        intent.putExtra("objetMedecin",medecin)
+        it.context.startActivity(intent)
     }
 }
