b4667e14e69692ea515d5996ea65d72d03d51c55
jules-fogeron <jules.fogeron@lyceeastier.com>
2024-02-08 14:27:46 +0100

    opti import

--- app/src/main/java/com/example/ecf_app/retrofit/Api.kt
+++ app/src/main/java/com/example/ecf_app/retrofit/Api.kt
@@ -8,10 +8,7 @@ import com.example.ecf_app.json.ListPrescription
 import com.example.ecf_app.json.Medecin
 import com.example.ecf_app.json.Prescription
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.Field
 import retrofit2.http.GET
-import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface Api {
