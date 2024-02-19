5505e15c349cb784008dc5a98cff2cb27fece7a2
jules-fogeron <jules.fogeron@lyceeastier.com>
2024-02-19 09:15:42 +0100

    changer adresse url api

--- app/src/main/java/com/example/ecf_app/retrofit/ApiAdapteur.kt
+++ app/src/main/java/com/example/ecf_app/retrofit/ApiAdapteur.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiAdapteur {
-    private const val BASE_URL = "http://192.168.1.38/ECF-Web/api/"
+    private const val BASE_URL = "http://soignemoi.julesfogeron.com/ECF-Web/api/"
     val apiClient: Api = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .client(OkHttpClient())
